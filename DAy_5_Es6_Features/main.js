c42ad9a2a92d8ef56cfa526103bdc0e8d950f662
Rubini <rubinibalusamy@gmail.com>
2026-01-04 22:59:11 +0530

    update

---
+++ DAy_5_Es6_Features/main.js
@@ -0,0 +1,7 @@
+import multiply, { divide } from './mathfunction.js'; // Note the .js
+
+const result = multiply(20, 10);
+const result1 = divide(20, 5);
+
+console.log('The Multiplication result is', result); // 200
+console.log('The Division result is', result1);      // 4
---
+++ DAy_5_Es6_Features/mathfunction.js
@@ -0,0 +1,5 @@
+const multiply=(a,b)=> a*b
+
+export default multiply
+
+export const divide=(a,b)=> a/b
